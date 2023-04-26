b16cb0e3a19dd67997182f7378d22fdcd90d8a9f
hcags <120849679+hcags@users.noreply.github.com>
2023-04-26 14:41:30 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,35 @@
-
+basic.forever(function () {
+    if (input.compassHeading() >= 135 && input.compassHeading() <= 225) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 45 && input.compassHeading() <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 225 && input.compassHeading() <= 315) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 45 && input.compassHeading() <= 315) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
