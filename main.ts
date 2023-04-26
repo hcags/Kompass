190d04a9b71f3481bb842b5727fc91c4941f9570
hcags <120849679+hcags@users.noreply.github.com>
2023-04-26 14:41:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,35 @@
-if (true) {
-	
-} else if (false) {
-	
-} else if (false) {
-	
-} else if (false) {
-	
-} else if (false) {
-	
-} else {
-	
-}
+basic.forever(function () {
+    if (input.compassHeading() >= 135 && input.compassHeading() <= 225) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 45 && input.compassHeading() <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 225 && input.compassHeading() <= 315) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() >= 45 || input.compassHeading() <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
